d9692ba94673e4a88b8d767049871d5d96f67e8f
shockleylee <shockleylee@gmail.com>
2016-04-26 21:20:18 -0400

    ds

--- impute.m
+++ impute.m
@@ -1,34 +1,38 @@
 %function [pred_out] = impute(w1_M1, w1_P1, X);
-load moviedata_sub
+load pmf_weight
+load moviedata_all
 %[I,J,V]=find(X);
 %train_vec = [I,J,V];
 mean_rating = mean(train_vec(:,3));
-empties = max(I)*max(J) - train_vec(size,1);
+X = full(sparse(train_vec(:,1), train_vec(:,2), train_vec(:,3)));
+n_users = size(X,1);
+n_items = size(X,2);
+nnz(X)
+empties = n_users*n_items - nnz(X);
 imputed_vec = zeros(empties, 3);
 imputed = 0;
-for i = 1:max(I)
-	for j = 1:max(J)
-		B = ismember((i,j),train_vec(:,1:2), 'row')
-		if B(1) > 0
+for i = 1:n_users
+	for j = 1:n_items
+		if X(i,j) ~= 0
 			imputed = imputed + 1;
-			imputed_vec(imputed,1:2) = (i,j);
+			imputed_vec(imputed,1:2) = [i,j];
 		end
 	end
 end
-assert imputed == empties;
+
 %%% Make predicitions on the validation data
 
 aa_p   = double(imputed_vec(:,1));
 aa_m   = double(imputed_vec(:,2));
 %rating = double(imputed_vec(:,3));
 
-pred_out = sum(w1_M1_sample(aa_m,:).*w1_P1_sample(aa_p,:),2) + mean_rating;
+pred_out = sum(w1_M1(aa_m,:).*w1_P1(aa_p,:),2) + mean_rating;
 ff = find(pred_out>5); pred_out(ff)=5;
 ff = find(pred_out<1); pred_out(ff)=1;
 imputed_vec(:,3) = pred_out;
 all_vec = cat(train_vec, imputed_vec, 1);
-ImX = sparse(all_vec(:,1), all_vec(:,2), all_vec(:,3))
-save Imx.mat Imx
+Im_X = sparse(all_vec(:,1), all_vec(:,2), all_vec(:,3))
+save Imx.mat Im_x
 
 
 
